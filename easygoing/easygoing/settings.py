afa2f063bb156024e5eb729c16d6d73d81656086
Merfarion <64864827+Merfarion@users.noreply.github.com>
2021-03-31 19:58:08 +0300

    пробуем найти simple

--- easygoing/easygoing/settings.py
+++ easygoing/easygoing/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    'posts',
+    'simple'
 ]
 
 MIDDLEWARE = [
@@ -78,8 +77,12 @@ WSGI_APPLICATION = 'easygoing.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'science',
+        'USER':'postgres',
+        'PASSWORD': '2468',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
