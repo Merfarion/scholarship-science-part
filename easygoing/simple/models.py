d9f9eb4b4bb8b60fa899097c8d46ecec34e0d1a5
Merfarion <64864827+Merfarion@users.noreply.github.com>
2021-04-01 14:49:20 +0300

    ломаем проект

--- easygoing/simple/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-
-class MyProject(models.Model):
-    status = models.TextField(max_length=200, verbose_name='Статус проекта', default=' ')
-    place = models.IntegerField(verbose_name='Занятое место', default=0)
