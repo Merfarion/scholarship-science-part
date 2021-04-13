c8954ade3b9be2b498186e95ca18638fe1ad81c6
Merfarion <64864827+Merfarion@users.noreply.github.com>
2021-04-13 21:54:41 +0300

    make field "scores"

--- Scientific/models.py
+++ Scientific/models.py
@@ -4,35 +4,48 @@ from django.db import models
 class MyProject(models.Model):
     status = models.TextField(max_length=200, verbose_name='Статус проекта', default=' ')
     place = models.IntegerField(verbose_name='Занятое место', default=0)
+    scores = models.IntegerField(default=-1)
+
 
 '''
 Название награды (приза) за результаты НИР, проводимой студентом (доклады на конференции не учитываются)
 '''
+
+
 class Scientific_Research_Work(models.Model):
     title = models.TextField()
     place = models.TextField()
     individual_team = models.BooleanField()
     RTU_reward = models.BooleanField()
     date = models.TextField()
+    scores = models.IntegerField(default=-1)
+
 
 '''
 Название документа, удостоверяющегоисключительное право студента на достигнутый им научный 
 (научно-методический, научно-технический, научно-творческий) результат интеллектуальной деятельности (патент, свидетельство)
 '''
+
+
 class Patent(models.Model):
     title = models.TextField()
     individual_team = models.BooleanField()
     RTU_reward = models.BooleanField()
     date = models.TextField()
+    scores = models.IntegerField(default=-1)
+
 
 '''
 Название гранта на выполнение НИР (только для получателей гранта, исполнители гранта не учитываются
 '''
+
+
 class Grant(models.Model):
     title = models.TextField()
     individual_team = models.BooleanField()
     RTU_reward = models.BooleanField()
     date = models.TextField()
+    scores = models.IntegerField(default=-1)
 
 
 '''
@@ -40,9 +53,12 @@ class Grant(models.Model):
 всероссийском,  ведомственном  или региональном издании, в издании Университетаили иной 
 организации в течение 1-ого года, предшествующего назначению повышенной государственной академической стипендии
 '''
+
+
 class Publications(models.Model):
     title = models.TextField()
     volume_title = models.TextField()
     level = models.TextField()
     authors_quantity = models.IntegerField()
     date = models.TextField()
+    scores = models.IntegerField(default=-1)
