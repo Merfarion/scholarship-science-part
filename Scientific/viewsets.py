4cf20e5cc399bfdeff6f3298c0ad064f2d2d3a53
Argon <70897099+Gonets-Artem@users.noreply.github.com>
2021-04-15 10:09:36 +0300

    Delete Myproject

--- Scientific/viewsets.py
+++ Scientific/viewsets.py
@@ -1,7 +1,7 @@
-from rest_framework import viewsets
-from . import models
-from . import serializers
+# from rest_framework import viewsets
+# from . import models
+# from . import serializers
 
-class MyprojectViewset (viewsets.ModelViewSet):
-    queryset = models.MyProject.objects.all()
-    serializer_class = serializers.MyProjectSerializer
+# class MyprojectViewset (viewsets.ModelViewSet):
+#     queryset = models.MyProject.objects.all()
+#     serializer_class = serializers.MyProjectSerializer
