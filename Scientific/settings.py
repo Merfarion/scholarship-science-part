62ab480fd4d1249e1f82ddf756f46f264393bf10
Merfarion <64864827+Merfarion@users.noreply.github.com>
2021-04-22 00:02:52 +0300

    Update my hell code

--- Scientific/settings.py
+++ Scientific/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ovl-m31rd^6340=6uso6$ssz20#s9d5y=6w1_om5f=sqp3xu37'
-
+# SECRET_KEY = 'ovl-m31rd^6340=6uso6$ssz20#s9d5y=6w1_om5f=sqp3xu37'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['science-cli.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['science-cli.herokuapp.com']#, '127.0.0.1'
 
 
 # Application definition
